d785cdfae55b309b6a92fb02c3c135af4dd412d2
Bulat <bulat5512@yandex.ru>
2020-05-17 16:39:20 +0300

    modify 2 for 12

--- 2/GeneratorCls.php
+++ 2/GeneratorCls.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class generatorClass
+class GeneratorCls
 {
     public $input = null;
     public $result = null;
--- 2/index.php
+++ 2/index.php
@@ -1,8 +1,9 @@
 <?php
 readfile('index.html');
-require "generator.php";
-
-$h = new generatorClass($_POST['code']);
-$h->run();
+require "GeneratorCls.php";
+if(isset($_POST['code'])) {
+    $h = new GeneratorCls($_POST['code']);
+    $h->run();
+}
 
 
